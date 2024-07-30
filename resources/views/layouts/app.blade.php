5c53bf1bb629a75ddd549771f043c800164f15c5
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-07-31 06:04:46 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                         <li class="nav-item dropdown d-flex">
                             <a href="/cart" class="nav-link">Keranjang</a>
                             <a href="/transaction" class="nav-link">Transaksi</a>
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" role="button"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
